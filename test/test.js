a1873cda62b293c5beedf42cdbffe4948bca657e
ferno <actinium@gmail.com>
2017-01-29 17:57:04 +0000

    Language-agnostic test case files
    These files should make it easier to port `base65536`, complete with its
    test cases, to other programming languages. All you need to do is port
    the adapter.

--- test/test.js
+++ test/test.js
@@ -2,106 +2,58 @@
 
 var base65536 = require("./../index.js");
 var fs = require("fs");
-
-// Exhaustively try all single-byte buffers
+var glob = require("glob");
 
 var forms = ["NFC", "NFD", "NFKC", "NFKD"];
 
-for(var b = 0; b < 256; b++) {
-	var buf1 = new Buffer([b]);
-	var str1 = base65536.encode(buf1);
-	forms.forEach(function(form) {
-		if(str1.normalize(form) !== str1) {
-			throw new Error("String failed to survive normalization! " + str1);
+glob("./test/pairs/**/*.bin", function(err, files) {
+	if(err) {
+		throw Error(err);
+	}
+	files.forEach(function(fileName) {
+		var caseName = fileName.substring(0, fileName.length - ".bin".length);
+		console.log(caseName);
+		var binary = fs.readFileSync(caseName + ".bin");
+		var text = fs.readFileSync(caseName + ".txt", "utf8");
+		if(base65536.encode(binary) !== text) {
+			console.error("Input binary", binary);
+			console.error("Expected text", text);
+			console.error("Actual text", base65536.encode(binary));
+			throw Error("Encode error");
 		}
-	});
-	var buf2 = base65536.decode(str1);
-	if(!buf1.equals(buf2)) {
-		throw new Error("Bad buffer round trip! " + String(b));
-	}
-}
-
-// What if there's a rogue end-of-stream char in there?
-var bad = base65536.encode(new Buffer([37])) + base65536.encode(new Buffer([19]));
-try {
-	base65536.decode(bad);
-	throw new Error("This should have failed");
-} catch(e) {
-	if(e.message === "This should have failed") {
-		throw e;
-	}
-	// otherwise OK
-}
-
-// This one failed at first, exposing a defect now fixed
-var firstDefectStr = base65536.encode(new Buffer([0, 110]));
-console.log(firstDefectStr.codePointAt(0) === 67072);
-console.log(firstDefectStr.codePointAt(0) === 0x10600);
-console.log(firstDefectStr.length === 2);
-console.log(firstDefectStr.charCodeAt(0) === 0xD801);
-console.log(firstDefectStr.charCodeAt(1) === 0xDE00);
-console.log(firstDefectStr === "\uD801\uDE00");
-var firstDefectBuf = base65536.decode(firstDefectStr);
-console.log(firstDefectBuf.equals(new Buffer([0, 110])));
-
-// Exhaustively try all two-byte buffers (takes a moment or two)
-for(var b1 = 0; b1 < 256; b1++) {
-	for(var b2 = 0; b2 < 256; b2++) {
-		var buf1 = new Buffer([b1, b2]);
-		var str1 = base65536.encode(buf1);
+		if(!base65536.decode(text).equals(binary)) {
+			console.error("Input text", text);
+			console.error("Expected binary", binary);
+			console.error("Actual binary", base65536.decode(text));
+			throw Error("Decode error");
+		}
 		forms.forEach(function(form) {
-			if(str1.normalize(form) !== str1) {
-				throw new Error("String failed to survive normalizetion! " + str1);
+			if(text.normalize(form) !== text) {
+				throw new Error("String failed to survive " + form + " normalization! " + text);
 			}
 		});
-		var buf2 = base65536.decode(str1);
-		if(!buf1.equals(buf2)) {
-			throw new Error("Bad buffer round trip! " + String(b1) + "," + String(b2));
-		}
-	}
-}
-
-// Try some real binary samples, including a few images
-
-var sampleFiles = [
-	"./test/sample-files/everyByte",
-	"./test/sample-files/everyPairOfBytes",
-	"./test/sample-files/lena_std.tif"
-];
-
-sampleFiles.forEach(function(sampleFile) {
-	var buf1 = fs.readFileSync(sampleFile);
-	var str1 = base65536.encode(buf1);
-	forms.forEach(function(form) {
-		if(str1.normalize(form) !== str1) {
-			throw new Error("String failed to survive normalization! " + str1);
-		}
 	});
-	var buf2 = base65536.decode(str1);
-	if(!buf1.equals(buf2)) {
-		throw new Error("Buffers differ!");
-	}
+
+	glob("./test/bad/**/*.txt", function(err, files) {
+		if(err) {
+			throw Error(err);
+		}
+		files.forEach(function(fileName) {
+			var caseName = fileName.substring(0, fileName.length - ".txt".length);
+			console.log(caseName);
+			var text = fs.readFileSync(caseName + ".txt", "utf8");
+			var threw;
+			try {
+				base65536.decode(text);
+				threw = false;
+			} catch(e) {
+				threw = true;
+			}
+			if(!threw) {
+				throw new Error("String should have failed to decode!" + text);
+			}
+		});
+	});
 });
 
-// Gee I hope this example code works
-(function() {
-	var buf = new Buffer("hello world");
-
-	var str = base65536.encode(buf);
-	console.log(str === "驨ꍬ啯𒁷ꍲᕤ");
-
-	var buf2 = base65536.decode(str);
-	console.log(buf.equals(buf2));
-}());
-
-(function() {
-	var str = base65536.encode(new Buffer([255, 255]));
-	console.log(str === "\uD861\uDDFF");
-	console.log(str.codePointAt(0) === 165375); // U+285FF
-	console.log(str === "𨗿");
-	console.log(str.length === 2);
-	console.log(str.charCodeAt(0) === 55393);
-	console.log(str.charCodeAt(1) === 56831);
-}());
-
-console.log("OK");
+// TODO: use an actual JS assertion library or something
