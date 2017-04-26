ea8c6d2a8a58d6b6a19fdd8c163364b52bb56d70
ferno <actinium@gmail.com>
2017-04-26 17:01:47 +0100

    Add a nominal little Gulp build
    * The Gulp task `build` simply copies the input to the new output
    directory, `dist`, although more work is planned.
    * An NPM script was added which carries out this build.
    * An NPM script was added to ensure that this build is carried out prior
    to publication.
    * `dist` is now ignored by Git.
    * Almost everything other than `dist` is now ignored by `npm publish`.
    
    I'm having a really hard time figuring out what best practice in this
    area is. NPM's documentation leaves a lot of these choices rather
    open-ended.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var del = require('del')
+var gulp = require('gulp')
+var gulpRename = require('gulp-rename');
+var runSequence = require('run-sequence')
+
+gulp.task('delete', function () {
+  del.sync('./dist')
+})
+
+gulp.task('js', function () {
+  return gulp
+    .src('./src/index.js')
+    .pipe(gulpRename('base65536.js'))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('build', function (cb) {
+  runSequence('delete', 'js', cb)
+})
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch('./src/**/*.*', ['build'])
+})
