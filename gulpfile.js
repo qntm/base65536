0e5837c28177f177cca045b494df4a59e57e0a81
ferno <actinium@gmail.com>
2017-05-03 21:40:03 +0100

    Minified distribution
    Using Uglify. Also modified the tests to test both the original and
    minified versions, because honestly I distrust my choices of
    minification flags.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 var del = require('del')
 var gulp = require('gulp')
 var gulpJasmine = require('gulp-jasmine')
+var gulpRename = require('gulp-rename')
 var gulpStandard = require('gulp-standard')
 var gulpTslint = require('gulp-tslint')
 var gulpTypescript = require('gulp-typescript')
+var gulpUglify = require('gulp-uglify')
 var runSequence = require('run-sequence')
 var tslint = require('tslint')
 
@@ -60,8 +62,20 @@ gulp.task('ts', function () {
     .pipe(gulp.dest('./dist'))
 })
 
+gulp.task('uglify', function () {
+  return gulp
+    .src('./dist/base65536.js')
+    .pipe(gulpRename('base65536.min.js'))
+    .pipe(gulpUglify({
+      mangle: {
+        toplevel: true
+      }
+    }))
+    .pipe(gulp.dest('./dist'))
+})
+
 gulp.task('build', function (cb) {
-  runSequence('delete', 'standard', 'ts', 'jasmine', cb)
+  runSequence('delete', 'standard', 'ts', 'uglify', 'jasmine', cb)
 })
 
 gulp.task('watch', ['build'], function () {
@@ -69,11 +83,8 @@ gulp.task('watch', ['build'], function () {
 })
 
 // TODO
-// Minified distribution
-// Test minified version too (?)
 // code coverage
 // convert Jasmine tests to TypeScript (?)
 //   (would probably have to test the TS code directly instead)
 // support wrapping output after certain number of columns
 // bin/base65536.js command a la <https://linux.die.net/man/1/base64>
-
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -2,153 +2,162 @@
 
 /* eslint-env jasmine */
 
-var base65536 = require('./../dist/base65536.js') // test the built JS file
-var fs = require('fs')
+const base65536 = require('./../dist/base65536.js') // test the built JS file
+const base65536min = require('./../dist/base65536.min.js')
+const fs = require('fs')
 
-describe('base65536', function () {
-  describe('success cases', function () {
-    var caseDir = './spec/pairs'
-    var caseNames = [
-      'demo',
-      'firstDefect',
-      'hatetris-wrs/hatetris-wr',
-      'hatetris-wrs/hatetris-wr-rle',
-      'hatetris-wrs/hatetris-wr-rle2',
-      'sample-files/everyByte',
-      'sample-files/everyPairOfBytes',
-      'sample-files/lena_std.tif',
-      'empty'
-    ]
-    var b
-    for (b = 0; b < 1 << 8; b++) {
-      caseNames.push('single-bytes/case' + String(b))
-    }
-    for (b = 0; b < 1 << 8; b++) {
-      caseNames.push('doubled-bytes/case' + String(b) + '-' + String(b))
-    }
+const modules = {
+  base65536: base65536,
+  base65536min: base65536min
+}
 
-    caseNames = caseNames.map(function (caseName) {
-      return caseDir + '/' + caseName
-    })
+Object.keys(modules).forEach(function (moduleName) {
+  const module = modules[moduleName]
 
-    describe('encode', function () {
-      caseNames.forEach(function (caseName) {
-        var binaryFileName = caseName + '.bin'
-        var textFileName = caseName + '.txt'
-
-        it(binaryFileName + ' to ' + textFileName, function () {
-          var binary = fs.readFileSync(binaryFileName)
-          var text = fs.readFileSync(textFileName, 'utf8')
-          expect(base65536.encode(binary)).toBe(text)
-        })
-      })
-    })
-
-    describe('decode', function () {
-      caseNames.forEach(function (caseName) {
-        var textFileName = caseName + '.txt'
-        var binaryFileName = caseName + '.bin'
-
-        it(textFileName + ' to ' + binaryFileName, function () {
-          var text = fs.readFileSync(textFileName, 'utf8')
-          var binary = fs.readFileSync(binaryFileName)
-          expect(base65536.decode(text).equals(binary)).toBe(true)
-        })
-      })
-    })
-
-    describe('ignoreGarbage', function () {
-      var caseDir = './spec/ignoreGarbage'
-      var caseNames = [
-        'abc',
-        'continuationAtEnd',
-        'lineBreak',
-        'lineBreaks',
-        'quoted',
-        'randomAlphanumericInterference',
-        'spaceAfter',
-        'spaceBefore',
-        'spacesEverywhere'
+  describe(moduleName, function () {
+    describe('success cases', function () {
+      const caseDir = './spec/pairs'
+      let caseNames = [
+        'demo',
+        'firstDefect',
+        'hatetris-wrs/hatetris-wr',
+        'hatetris-wrs/hatetris-wr-rle',
+        'hatetris-wrs/hatetris-wr-rle2',
+        'sample-files/everyByte',
+        'sample-files/everyPairOfBytes',
+        'sample-files/lena_std.tif',
+        'empty'
       ]
+      for (let b = 0; b < 1 << 8; b++) {
+        caseNames.push('single-bytes/case' + String(b))
+      }
+      for (let b = 0; b < 1 << 8; b++) {
+        caseNames.push('doubled-bytes/case' + String(b) + '-' + String(b))
+      }
+
       caseNames = caseNames.map(function (caseName) {
         return caseDir + '/' + caseName
       })
 
-      describe('by default', function () {
+      describe('encode', function () {
         caseNames.forEach(function (caseName) {
-          var textFileName = caseName + '.txt'
-          it('fails to decode ' + textFileName, function () {
-            var text = fs.readFileSync(textFileName, 'utf8')
+          const binaryFileName = caseName + '.bin'
+          const textFileName = caseName + '.txt'
+
+          it(binaryFileName + ' to ' + textFileName, function () {
+            const binary = fs.readFileSync(binaryFileName)
+            const text = fs.readFileSync(textFileName, 'utf8')
+            expect(module.encode(binary)).toBe(text)
+          })
+        })
+      })
+
+      describe('decode', function () {
+        caseNames.forEach(function (caseName) {
+          const textFileName = caseName + '.txt'
+          const binaryFileName = caseName + '.bin'
+
+          it(textFileName + ' to ' + binaryFileName, function () {
+            const text = fs.readFileSync(textFileName, 'utf8')
+            const binary = fs.readFileSync(binaryFileName)
+            expect(module.decode(text).equals(binary)).toBe(true)
+          })
+        })
+      })
+
+      describe('ignoreGarbage', function () {
+        const caseDir = './spec/ignoreGarbage'
+        let caseNames = [
+          'abc',
+          'continuationAtEnd',
+          'lineBreak',
+          'lineBreaks',
+          'quoted',
+          'randomAlphanumericInterference',
+          'spaceAfter',
+          'spaceBefore',
+          'spacesEverywhere'
+        ]
+        caseNames = caseNames.map(function (caseName) {
+          return caseDir + '/' + caseName
+        })
+
+        describe('by default', function () {
+          caseNames.forEach(function (caseName) {
+            const textFileName = caseName + '.txt'
+            it('fails to decode ' + textFileName, function () {
+              const text = fs.readFileSync(textFileName, 'utf8')
+              expect(function () {
+                module.decode(text)
+              }).toThrow()
+            })
+          })
+        })
+
+        describe('`false`', function () {
+          caseNames.forEach(function (caseName) {
+            const textFileName = caseName + '.txt'
+            it('fails to decode ' + textFileName, function () {
+              const text = fs.readFileSync(textFileName, 'utf8')
+              expect(function () {
+                module.decode(text, false)
+              }).toThrow()
+            })
+          })
+        })
+
+        describe('`true`', function () {
+          caseNames.forEach(function (caseName) {
+            const textFileName = caseName + '.txt'
+            const binaryFileName = caseName + '.bin'
+            it('fails to decode ' + textFileName, function () {
+              const text = fs.readFileSync(textFileName, 'utf8')
+              const binary = fs.readFileSync(binaryFileName)
+              expect(module.decode(text, true).equals(binary)).toBe(true)
+            })
+          })
+        })
+      })
+
+      const forms = ['NFC', 'NFD', 'NFKC', 'NFKD']
+      forms.forEach(function (form) {
+        describe(form + ' normalization', function () {
+          caseNames.forEach(function (caseName) {
+            const textFileName = caseName + '.txt'
+            it(textFileName + ' survives', function () {
+              const text = fs.readFileSync(textFileName, 'utf8')
+              expect(text.normalize(form)).toBe(text)
+            })
+          })
+        })
+      })
+    })
+
+    describe('failure cases', function () {
+      const caseDir = './spec/bad'
+      const caseNames = [
+        'abc',
+        'endOfStreamBeginsStream',
+        'endOfStreamMidStream',
+        'endOfStreamMidStreamEarlier',
+        'eosThenJunk',
+        'junkOnEnd',
+        'lineBreak',
+        'rogueEndOfStreamChar',
+        'twoEndsOfStream'
+      ].map(function (caseNames) {
+        return caseDir + '/' + caseNames
+      })
+
+      describe('cannot decode', function () {
+        caseNames.forEach(function (caseName) {
+          const textFileName = caseName + '.txt'
+          it(textFileName, function () {
+            const text = fs.readFileSync(textFileName, 'utf8')
             expect(function () {
-              base65536.decode(text)
+              module.decode(text)
             }).toThrow()
           })
-        })
-      })
-
-      describe('`false`', function () {
-        caseNames.forEach(function (caseName) {
-          var textFileName = caseName + '.txt'
-          it('fails to decode ' + textFileName, function () {
-            var text = fs.readFileSync(textFileName, 'utf8')
-            expect(function () {
-              base65536.decode(text, false)
-            }).toThrow()
-          })
-        })
-      })
-
-      describe('`true`', function () {
-        caseNames.forEach(function (caseName) {
-          var textFileName = caseName + '.txt'
-          var binaryFileName = caseName + '.bin'
-          it('fails to decode ' + textFileName, function () {
-            var text = fs.readFileSync(textFileName, 'utf8')
-            var binary = fs.readFileSync(binaryFileName)
-            expect(base65536.decode(text, true).equals(binary)).toBe(true)
-          })
-        })
-      })
-    })
-
-    var forms = ['NFC', 'NFD', 'NFKC', 'NFKD']
-    forms.forEach(function (form) {
-      describe(form + ' normalization', function () {
-        caseNames.forEach(function (caseName) {
-          var textFileName = caseName + '.txt'
-          it(textFileName + ' survives', function () {
-            var text = fs.readFileSync(textFileName, 'utf8')
-            expect(text.normalize(form)).toBe(text)
-          })
-        })
-      })
-    })
-  })
-
-  describe('failure cases', function () {
-    var caseDir = './spec/bad'
-    var caseNames = [
-      'abc',
-      'endOfStreamBeginsStream',
-      'endOfStreamMidStream',
-      'endOfStreamMidStreamEarlier',
-      'eosThenJunk',
-      'junkOnEnd',
-      'lineBreak',
-      'rogueEndOfStreamChar',
-      'twoEndsOfStream'
-    ].map(function (caseNames) {
-      return caseDir + '/' + caseNames
-    })
-
-    describe('cannot decode', function () {
-      caseNames.forEach(function (caseName) {
-        var textFileName = caseName + '.txt'
-        it(textFileName, function () {
-          var text = fs.readFileSync(textFileName, 'utf8')
-          expect(function () {
-            base65536.decode(text)
-          }).toThrow()
         })
       })
     })
