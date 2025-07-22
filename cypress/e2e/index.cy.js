f61a83e21d1e579127c31828d56babe0890c291b
qntm <qntm@users.noreply.github.com>
2025-07-22 14:23:27 +0100

    Why do I have front-end tests?

--- cypress/e2e/index.cy.js
+++
@@ -1,8 +0,0 @@
-/* global describe, it, cy */
-
-describe('base65536 in the browser', () => {
-  it('works', () => {
-    cy.visit('http://localhost:3000/server/index.html')
-    cy.contains('what the heck is up')
-  })
-})
