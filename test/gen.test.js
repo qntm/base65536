7daf58407378c09b3ef85d4566747593c0b8da13
qntm <qntm@users.noreply.github.com>
2025-12-22 21:52:04 +0000

    Update gen.test.js

--- test/gen.test.js
+++ test/gen.test.js
@@ -1,5 +1,6 @@
 import assert from 'node:assert/strict'
-import { describe, it } from 'mocha'
+import { describe, it } from 'node:test'
+
 import { paddingBlockStart, blockStarts, safeCodePoint, pairStrings } from '../scripts/gen.js'
 
 describe('gen', () => {
